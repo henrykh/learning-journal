c42debf197da9ff1d428d2b00c2db15239def93d
henrykh <henryhowes@gmail.com>
2015-02-05 21:48:51 -0800

    adds login helper and tests

--- test_journal.py
+++ test_journal.py
@@ -14,6 +14,7 @@ from cryptacular.bcrypt import BCRYPTPasswordManager
 
 TEST_DSN = 'dbname=test_learning_journal user=henryhowes'
 
+INPUT_BTN = '<input type="submit" value="Share" name="Share"/>'
 
 def init_db(settings):
     with closing(connect_db(settings)) as db:
@@ -264,3 +265,37 @@ def test_do_login_missing_params(auth_req):
         auth_req.params = params
         with pytest.raises(ValueError):
             do_login(auth_req)
+
+
+def login_helper(username, password, app):
+    """encapsulate app login for reuse in tests
+
+    Accept all status codes so that we can make assertions in tests
+    """
+    login_data = {'username': username, 'password': password}
+    return app.post('/login', params=login_data, status='*')
+
+
+def test_start_as_anonymous(app):
+    response = app.get('/', status=200)
+    actual = response.body
+    assert INPUT_BTN not in actual
+
+
+def test_login_success(app):
+    username, password = ('admin', 'secret')
+    redirect = login_helper(username, password, app)
+    assert redirect.status_code == 302
+    response = redirect.follow()
+    assert response.status_code == 200
+    actual = response.body
+    assert INPUT_BTN in actual
+
+
+def test_login_fails(app):
+    username, password = ('admin', 'wrong')
+    response = login_helper(username, password, app)
+    assert response.status_code == 200
+    actual = response.body
+    assert "Login Failed" in actual
+    assert INPUT_BTN not in actual
